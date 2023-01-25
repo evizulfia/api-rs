7d5940f1991aab4b9abe54213a2936dcb747cc91
Muhammad Husni Aziz <muhhusniaziz@gmail.com>
2023-01-25 07:54:54 +0700

    otomatis obat

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -94,10 +94,10 @@ class TransactionController extends Controller
 
             $obat = Obat::where('id_obat', $request->id_obat)->first();
             if($obat){
-                $transaction->id_obat   = $request->id_obat;
-                $transaction->harga = $obat->harga;
-                $transaction->nama_obat = $obat->nama_obat;
-                $transaction->total    = $obat->harga*$request->qty;
+                $transaction->id_obat       = $request->id_obat;
+                $transaction->harga         = $obat->harga;
+                $transaction->nama_obat     = $obat->nama_obat;
+                $transaction->total         = $obat->harga*$request->qty;
 
             }
             
